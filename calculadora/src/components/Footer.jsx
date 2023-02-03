65e2e9b88c648d5313f2c191f58d45f91f504c9b
Frederico Nogueira <fredericonogueira01@outlook.com>
2023-02-03 12:15:15 -0300

    Calculadora projeto final

---
+++ calculadora/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Footer.css'
+
+function Footer() {
+  return <footer>
+    <p>Frederico Nogueira | <b>Calculadora</b> &copy; 2023<br/>
+    Este é um projeto educacional</p>
+  </footer>
+}
+
+export default Footer
